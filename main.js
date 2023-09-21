61491c4db4da80a375796ab41f6343ae7a4628b5
amtulaleem <145044820+amtulaleem@users.noreply.github.com>
2023-09-21 10:45:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+//Complete the phrase
+console.log("hi");
+
+//Button 1- Lemon squeezy
+document.getElementById("btn1").addEventListener("click", btn1clicked);
+
+function btn1clicked() {
+  alert("... lemon squeezy");
+}
+//Button 2- Caqke
+document.getElementById("btn2").addEventListener("click", btn2clicked);
+function btn2clicked() {
+  alert("...cake");
+}
+
+//Button 3- Short
+document.getElementById("btn3").addEventListener("click", btn3clicked);
+function btn3clicked() {
+  alert("...short");
+}
+//Button 4- When you're having fun
+document.getElementById("btn4").addEventListener("click", btn4clicked);
+function btn4clicked() {
+  alert("...when you're having fun");
+}
+//Button 5- No gain
+document.getElementById("btn5").addEventListener("click", btn5clicked);
+function btn5clicked() {
+  alert("... No gain");
+}
